15655aeb3eaaade26f4470f437e1e7263797a05c
Ivan Volynets <sobwoofer8@gmail.com>
2021-01-18 19:28:30 +0200

    fixed docker files

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -2,15 +2,24 @@
 
 namespace App\Controller;
 
-
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
-class AboutController
+class AboutController extends AbstractController
 {
-    public function index(): Response
+    /**
+     * @Route("/", name="about")
+     */
+    public function index(Request $request): Response
     {
         $text = 'hello world';
 
-        return new Response('<h1>' . $text . '</h1>');
+        if ($request->get('hello')) {
+            $text = '<h1>hello</h1>';
+        }
+
+        return new Response($text . '<img src="/images/construction.jpg">');
     }
 }
